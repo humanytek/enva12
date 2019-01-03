16573254103043074b22e948651995465c0c835b
aespinosa23 <41025990+aespinosa23@users.noreply.github.com>
2019-01-03 08:37:40 -0600

    Create __manifest__.py

---
+++ prod_nova/report_remission/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Report Remission',
+    'author': 'EMPAQUES NOVA isc_dcm',
+    'website': 'http://www.empaquesnova.com.mx',
+    'category': 'Uncategorized',
+    'version': '1.0.0',
+    'depends': [
+        'stock',
+        'sale_management',
+    ],
+    'data': [
+        'reports/report_remission.xml',
+        'views/sale.xml',
+    ],
+}
