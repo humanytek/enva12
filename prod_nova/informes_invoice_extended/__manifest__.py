b87833c43d989cd13ce4e7177ce2191fa0ed95e3
aespinosa23 <41025990+aespinosa23@users.noreply.github.com>
2019-10-24 14:58:55 -0500

    informes invoice extended b

---
+++ prod_nova/informes_invoice_extended/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Informes Invoice Extended',
+    'author': 'EMPAQUES NOVA',
+    'website': 'http://www.empaquesnova.com.mx',
+    'category': 'Account',
+    'version': '1.3.0',
+    'depends': [
+        'informes_invoice',
+    ],
+    'data': [],
+}
