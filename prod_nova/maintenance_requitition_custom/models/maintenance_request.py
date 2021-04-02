47774bc2d4fe661f9a3b932620d9f26c78665386
angel <aespinosa@empaquesnova.com.mx>
2021-04-02 11:47:49 -0600

    PRUEBA

--- prod_nova/maintenance_requitition_custom/models/maintenance_request.py
+++ prod_nova/maintenance_requitition_custom/models/maintenance_request.py
@@ -53,6 +53,6 @@ class MaintenanceRequest(models.Model):
         self.priority=mantto_requisition.priority
         self.note=mantto_requisition.note
         self.request_date=mantto_requisition.date_request
-        self.user_id.id=mantto_requisition.assigned_id.id
+        self.user_id=mantto_requisition.assigned_id
         # _logger.info('USUARIO %s', mantto_requisition.assigned_id.id)
         self.area_id=mantto_requisition.area_id
