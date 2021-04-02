d8cbf94e1bfdf791bdd06f86fc5e18d328e07362
angel <aespinosa@empaquesnova.com.mx>
2021-04-02 07:01:52 -0600

    user ids

--- prod_nova/maintenance_requitition_custom/models/maintenance_area.py
+++ prod_nova/maintenance_requitition_custom/models/maintenance_area.py
@@ -15,5 +15,5 @@ class MaintenanceArea(models.Model):
     )
 
     user_ids = fields.Many2many(
-        'res.users', 'maintenance_area_users_rel', string="Usuarios"
+        comodel_name='res.users', string="Usuarios"
     )
