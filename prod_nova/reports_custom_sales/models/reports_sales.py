3a2529882fb680e79491aab1c36a227554566ef9
angel <aespinosa@empaquesnova.com.mx>
2021-04-07 13:14:40 -0500

    NAME CHECK

--- prod_nova/reports_custom_sales/models/reports_sales.py
+++ prod_nova/reports_custom_sales/models/reports_sales.py
@@ -86,7 +86,7 @@ class ReportsSales(models.AbstractModel):
 
         if invoices:
             for invoice in invoices:
-                budget=self.env['trend.budget.sales'].search([('name.id','=',157)])
+                budget=self.env['trend.budget.sales'].search([('name','=','AIRSYSTEM S.A. DE C.V.' )])
                  lines.append({
                         'id': str(invoice[0]),
                         'name': str(invoice[0]),
