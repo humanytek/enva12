4250f4893ef85d57bf6911efbd43a8cd9f70a177
angel <aespinosa@empaquesnova.com.mx>
2021-04-07 13:09:46 -0500

    ERROR SERCH

--- prod_nova/reports_custom_sales/models/reports_sales.py
+++ prod_nova/reports_custom_sales/models/reports_sales.py
@@ -86,7 +86,7 @@ class ReportsSales(models.AbstractModel):
 
         if invoices:
             for invoice in invoices:
-                budget=self.env['trend.budget.sales'].search([('name','=',157),('date_from','>=',date_from),('date_to','<=',date_to)])
+                budget=self.env['trend.budget.sales'].search([('name','=',157)])
                  lines.append({
                         'id': str(invoice[0]),
                         'name': str(invoice[0]),
