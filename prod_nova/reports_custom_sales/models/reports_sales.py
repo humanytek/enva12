bcb661276443257a1730937b0fed5a1c8e45043d
angel <aespinosa@empaquesnova.com.mx>
2021-04-08 12:06:01 -0500

    string to int

--- prod_nova/reports_custom_sales/models/reports_sales.py
+++ prod_nova/reports_custom_sales/models/reports_sales.py
@@ -53,7 +53,7 @@ class ReportsSales(models.AbstractModel):
                     LEFT JOIN product_template pt ON pt.id=pp.product_tmpl_id
                     LEFT JOIN account_invoice ai ON ai.id=ail.invoice_id
                     LEFT JOIN res_partner rp ON rp.id=ail.partner_id
-                    WHERE ai.state!='draft' AND ai.state!='cancel' AND ai.type='out_invoice' AND rp.id="""+partner_id+""" AND ai.date_applied >= '"""+date_from+"""' AND ai.date_applied <= '"""+date_to+"""'
+                    WHERE ai.state!='draft' AND ai.state!='cancel' AND ai.type='out_invoice' AND rp.id="""+int(partner_id)+""" AND ai.date_applied >= '"""+date_from+"""' AND ai.date_applied <= '"""+date_to+"""'
                     AND ai.user_id not in (90) AND pt.name not ilike 'ANTICIPO DE CLIENTE%' AND pt.name not ilike 'TRANSPORTACION%' AND pt.name not ilike 'CHATARRA%' AND pt.name not ilike 'PUB GRAL VTA CHATARRA%'
                     GROUP BY rp.id,rp.name
                     ORDER BY rp.name ASC
@@ -138,7 +138,7 @@ class ReportsSales(models.AbstractModel):
             for invoice in invoices:
                 budget=self._get_budget_sales(invoice[1], fields.Date.from_string(date_from),fields.Date.from_string(date_to))
                 invoices_line=self._invoice_line_partner(options,line_id,invoice[1])
-                
+
                 lines.append({
                         'id': str(invoice[0]),
                         'name': str(invoice[0]),
