8377b1b62a96c38447d594aca3c6af4027685d9f
angel <aespinosa@empaquesnova.com.mx>
2021-04-12 12:09:25 -0500

    ORDER BY

--- prod_nova/reports_custom_sales/models/reports_sales.py
+++ prod_nova/reports_custom_sales/models/reports_sales.py
@@ -139,7 +139,7 @@ class ReportsSales(models.AbstractModel):
                     LEFT JOIN trend_budget_sales tbs ON tbs.name=rp.id
                     WHERE ai.state!='draft' AND ai.state!='cancel' AND ai.type='out_invoice' AND ai.date_applied >= '"""+date_from+"""' AND ai.date_applied <= '"""+date_to+"""'
                     AND ai.user_id not in (90) AND pt.name not ilike 'ANTICIPO DE CLIENTE%' AND pt.name not ilike 'TRANSPORTACION%' AND pt.name not ilike 'CHATARRA%' AND pt.name not ilike 'PUB GRAL VTA CHATARRA%'
-                    GROUP BY rp.name,rp.id
+                    GROUP BY rp.name,rp.id,tbs.kg_per_month
                     )
                     UNION
                     (
