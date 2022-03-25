d2e3c9d5c4ab2e2f5a0f46d72153853a74a39315
Angel Espinosa <aespinosa@empaquesnova.com.mx>
2022-03-25 13:50:15 -0600

    update

--- prod_nova/reports_custom_payments/models/reports_payments.py
+++ prod_nova/reports_custom_payments/models/reports_payments.py
@@ -72,7 +72,7 @@ class ReportsPayments(models.AbstractModel):
                     WHERE am.date >= '"""+date_from+"""' AND am.date <= '"""+date_to+"""'
                     AND am.state in ('posted') AND ap.payment_type in ('outbound')
                     AND invoice.move_type in ('in_invoice', 'in_refund')
-                    ORDER BY rp.name,am.date,ap.payment_reference,am.ref
+                    ORDER BY ap.id,rp.name,am.date,ap.payment_reference,am.ref
 
 
         """
