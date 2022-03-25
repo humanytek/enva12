553cf26639cff5e89aeb6f3adbf9da2a004dc41e
Angel Espinosa <aespinosa@empaquesnova.com.mx>
2022-03-25 14:28:47 -0600

    update informes payments

--- prod_nova/reports_custom_payments/models/reports_payments.py
+++ prod_nova/reports_custom_payments/models/reports_payments.py
@@ -58,8 +58,12 @@ class ReportsPayments(models.AbstractModel):
                     JOIN account_move am ON am.id=ap.move_id
                     JOIN account_move_line line ON line.move_id = am.id
                     JOIN account_partial_reconcile part ON
+                        part.debit_move_id = line.id
+                        OR
                         part.credit_move_id = line.id
                     JOIN account_move_line counterpart_line ON
+                        part.debit_move_id = counterpart_line.id
+                        OR
                         part.credit_move_id = counterpart_line.id
                     JOIN account_move invoice ON invoice.id = counterpart_line.move_id
                     JOIN res_partner rp ON rp.id=ap.partner_id
