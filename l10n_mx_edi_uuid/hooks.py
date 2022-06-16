1a9b42f369954aa3ad3d39291b2ad7c77b5a2b2d
Moises Navarro <moisalejandro@gmail.com>
2022-06-16 14:19:16 -0500

    [REM] update_uuid from init hook

--- l10n_mx_edi_uuid/hooks.py
+++ l10n_mx_edi_uuid/hooks.py
@@ -6,4 +6,4 @@ from odoo import SUPERUSER_ID, api
 def post_init_hook(cr, registry):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        env['ir.attachment'].search([]).update_uuid()
+        # env['ir.attachment'].search([]).update_uuid()
