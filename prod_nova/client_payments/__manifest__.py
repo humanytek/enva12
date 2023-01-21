dafe6a40fd8b051b215d796f800e5b02c76a26d1
aespinosa23 <aespinosa@empaquesnova.com.mx>
2023-01-21 11:07:40 -0600

    client_payments

--- prod_nova/client_payments/__manifest__.py
+++ prod_nova/client_payments/__manifest__.py
@@ -9,7 +9,7 @@
         'l10n_mx_edi',
     ],
     'data': [
-        'reports/client_payment_report.xml',
+        # 'reports/client_payment_report.xml',
         'views/paymentclient.xml',
     ],
 }
